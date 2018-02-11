3bf386dac3605f661bf6bac0bdee1bfa7b7c2d8b
Zahi Savion <tsahi@netvision.net.il>
2018-02-11 15:48:01 +0200

    20180211Partialyy Image

--- app/src/main/java/com/ozs/simplekidsmath2/ChildMaintActivity.java
+++ app/src/main/java/com/ozs/simplekidsmath2/ChildMaintActivity.java
@@ -32,6 +32,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -43,8 +44,8 @@ public class ChildMaintActivity extends AppCompatActivity {
     public final  String PREF_PHOTO_PATH="pref_photo_path";
     public final int     IMAGE_MAX_SIZE = 1200000; // 1.2MP
 
-    public final  int  CHILD_IMAGE_WIDTH=400;
-    public final  int  CHILD_IMAGE_HEIGHT=400;
+    public final  int  CHILD_IMAGE_WIDTH=300;
+    public final  int  CHILD_IMAGE_HEIGHT=300;
 
     public final  int  CHOOSE_FROM_GALLERY=0;
     public final  int  CHOOSE_FROM_CAMERA=1;
@@ -67,6 +68,7 @@ public class ChildMaintActivity extends AppCompatActivity {
 
         etName= (EditText) findViewById(R.id.editTextName);
         ibPic = (ImageButton) findViewById(R.id.imageButtonPic);
+        m_ivLoadPic = (ImageView) findViewById(R.id.imageViewPic);
 
         if (savedInstanceState!=null)
         {
@@ -250,9 +252,6 @@ public class ChildMaintActivity extends AppCompatActivity {
     {
     }
 
-    /*
-     dispatchTakePictureIntent
-     */
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
@@ -333,16 +332,17 @@ public class ChildMaintActivity extends AppCompatActivity {
                     }
                 }
 
+                if (resultCode == RESULT_OK) {
+                    if (imageReturnedIntent != null) {
 
-                if (imageReturnedIntent != null) {
+                        if (imageReturnedIntent.hasExtra("data")) {
 
-                    if (imageReturnedIntent.hasExtra("data")) {
+                        }
+                    } else {
+
+                        setPic();
 
                     }
-                } else {
-
-                    setPic();
-
                 }
                 break;
         }
@@ -355,7 +355,8 @@ public class ChildMaintActivity extends AppCompatActivity {
     {
         try {
             DeleteOldPic(m_ModeId);
-            FileOutputStream out = openFileOutput("C1.png", Context.MODE_PRIVATE);
+            // We don't know the format
+            FileOutputStream out = openFileOutput(m_ModeId+".png", Context.MODE_PRIVATE);
             byte[] buffer = new byte[1024]; // Adjust if you want
             int bytesRead;
             while ((bytesRead = imageStream.read(buffer)) != -1)
@@ -445,6 +446,113 @@ public class ChildMaintActivity extends AppCompatActivity {
         SharedPreferences.Editor editor=sp.edit();
         editor.remove(PREF_PHOTO_PATH);
         editor.commit();
+    }
+
+    /**
+     * This method is responsible for solving the rotation issue if exist. Also scale the images to
+     * 1024x1024 resolution
+     *
+     * @param context       The current context
+     * @param selectedImage The Image URI
+     * @return Bitmap image results
+     * @throws IOException
+     */
+    public static Bitmap handleSamplingAndRotationBitmap(Context context, Uri selectedImage)
+            throws IOException {
+        int MAX_HEIGHT = 1024;
+        int MAX_WIDTH = 1024;
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        InputStream imageStream = context.getContentResolver().openInputStream(selectedImage);
+        BitmapFactory.decodeStream(imageStream, null, options);
+        imageStream.close();
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        imageStream = context.getContentResolver().openInputStream(selectedImage);
+        Bitmap img = BitmapFactory.decodeStream(imageStream, null, options);
+
+        img = rotateImageIfRequired(context, img, selectedImage);
+        return img;
+    }
+
+
+    /**
+     * Calculate an inSampleSize for use in a {@link BitmapFactory.Options} object when decoding
+     * bitmaps using the decode* methods from {@link BitmapFactory}. This implementation calculates
+     * the closest inSampleSize that will result in the final decoded bitmap having a width and
+     * height equal to or larger than the requested width and height. This implementation does not
+     * ensure a power of 2 is returned for inSampleSize which can be faster when decoding but
+     * results in a larger bitmap which isn't as useful for caching purposes.
+     *
+     * @param options   An options object with out* params already populated (run through a decode*
+     *                  method with inJustDecodeBounds==true
+     * @param reqWidth  The requested width of the resulting bitmap
+     * @param reqHeight The requested height of the resulting bitmap
+     * @return The value to be used for inSampleSize
+     */
+    private static int calculateInSampleSize(BitmapFactory.Options options,
+                                             int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee a final image
+            // with both dimensions larger than or equal to the requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+
+            // This offers some additional logic in case the image has a strange
+            // aspect ratio. For example, a panorama may have a much larger
+            // width than height. In these cases the total pixels might still
+            // end up being too large to fit comfortably in memory, so we should
+            // be more aggressive with sample down the image (=larger inSampleSize).
+
+            final float totalPixels = width * height;
+
+            // Anything more than 2x the requested pixels we'll sample down further
+            final float totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap) {
+                inSampleSize++;
+            }
+        }
+        return inSampleSize;
+    }
+
+
+    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
+
+        InputStream input = context.getContentResolver().openInputStream(selectedImage);
+        ExifInterface ei;
+        if (Build.VERSION.SDK_INT > 23)
+            ei = new ExifInterface(input);
+        else
+            ei = new ExifInterface(selectedImage.getPath());
+
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                return rotateImage(img, 90);
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                return rotateImage(img, 180);
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                return rotateImage(img, 270);
+            default:
+                return img;
+        }
     }
 
 }
