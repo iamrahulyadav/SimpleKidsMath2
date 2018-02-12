cf82a95d183246a3faa5fc28c7812fc5dbc7ad50
Zahi Savion <tsahi@netvision.net.il>
2018-02-12 20:42:22 +0200

    Working on Crop

--- app/src/main/java/com/ozs/simplekidsmath2/ChildMaintActivity.java
+++ app/src/main/java/com/ozs/simplekidsmath2/ChildMaintActivity.java
@@ -3,13 +3,18 @@ package com.ozs.simplekidsmath2;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,20 +26,28 @@ import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 public class ChildMaintActivity extends AppCompatActivity {
 
     private static final String STATE_NAME="STATE_NAME";
+    private static final String STATE_MODE="STATE_MODE";
+    private static final String STATE_UUID="STATE_UUID";
 
     boolean      m_isPermitted = true;
-    ImageView    m_ivLoadPic;
+
     String       m_Mode;
     String       m_ModeId;
     EditText     etName;
     ImageButton  ibPic;
     ChildrenList childList;
     AlertDialog  alert=null;
+
 
 
     @Override
@@ -52,26 +65,33 @@ public class ChildMaintActivity extends AppCompatActivity {
                 String txtTitle=savedInstanceState.getString(STATE_NAME);
                 etName.setText(txtTitle);
             }
-            else
+            if (savedInstanceState.containsKey(STATE_UUID))
             {
-                UpdateUIFromPreferences();
+                m_ModeId=savedInstanceState.getString(STATE_UUID);
+
             }
+            if (savedInstanceState.containsKey(STATE_MODE))
+            {
+                m_Mode=savedInstanceState.getString(STATE_MODE);
+            }
+
         }
         else {
             UpdateUIFromPreferences();
+            Intent myIntent=getIntent();
+            Bundle bundle=myIntent.getExtras();
+            m_Mode=bundle.getString(MainActivity.CHILD_MODE,"ADD");
+            m_ModeId=bundle.getString(MainActivity.CHILD_MODE_ID,"");
+
+            if (m_ModeId.trim().length()==0){
+                // Create new Temporary guid
+                UUID uuid = UUID.randomUUID();
+                String randomUUIDString = uuid.toString();
+                m_ModeId = randomUUIDString;
+            }
+
         }
 
-        Intent myIntent=getIntent();
-        Bundle bundle=myIntent.getExtras();
-        String m_Mode=bundle.getString(MainActivity.CHILD_MODE,"ADD");
-        String m_ModeId=bundle.getString(MainActivity.CHILD_MODE_ID,"");
-
-        if (m_ModeId.trim().length()==0){
-            // Create new Temporary guid
-            UUID uuid = UUID.randomUUID();
-            String randomUUIDString = uuid.toString();
-            m_ModeId = randomUUIDString;
-        }
 
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED &&
@@ -154,6 +174,8 @@ public class ChildMaintActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
         outState.putString(STATE_NAME,etName.getText().toString());
+        outState.putString(STATE_UUID,m_ModeId);
+        outState.putString(STATE_UUID,m_Mode);
     }
 
     @Override
@@ -163,6 +185,15 @@ public class ChildMaintActivity extends AppCompatActivity {
         {
             String txtTitle=savedInstanceState.getString(STATE_NAME);
             etName.setText(txtTitle);
+        }
+        if (savedInstanceState.containsKey(STATE_UUID))
+        {
+            m_ModeId=savedInstanceState.getString(STATE_UUID);
+
+        }
+        if (savedInstanceState.containsKey(STATE_MODE))
+        {
+            m_Mode=savedInstanceState.getString(STATE_MODE);
         }
     }
 
@@ -174,13 +205,14 @@ public class ChildMaintActivity extends AppCompatActivity {
 
     private void SavePreferences(Boolean IsFinish)
     {
+        SaveImage();
     }
     /** Start pick image activity with chooser. */
     public void onSelectImageClick(View view) {
         CropImage.activity()
                 .setGuidelines(CropImageView.Guidelines.ON)
-                .setActivityTitle("My Crop")
-                .setCropShape(CropImageView.CropShape.OVAL)
+                .setActivityTitle("Crop Child Image")
+                .setCropShape(CropImageView.CropShape.RECTANGLE)
                 .setCropMenuCropButtonTitle("Done")
                 .setRequestedSize(400, 400)
                 .setCropMenuCropButtonIcon(R.mipmap.ic_launcher)
@@ -200,6 +232,68 @@ public class ChildMaintActivity extends AppCompatActivity {
                         .show();
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+    protected void SaveImage(){
+
+        File rootPics = new File(getApplicationInfo().dataDir+File.separator+"pics");
+        File rootDownload = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        if (!rootPics.exists())
+        {
+            rootPics.mkdirs();
+        }
+
+
+        ImageView  imageview = (ImageView) findViewById(R.id.quick_start_cropped_image);
+        imageview.buildDrawingCache();
+        Bitmap bm=imageview.getDrawingCache();
+        OutputStream fOut = null;
+        Uri outputFileUri;
+        try {
+            PackageManager m = getPackageManager();
+            String s = getPackageName();
+            File root = new File(getApplicationInfo().dataDir + File.separator + "pics" + File.separator);
+            File sdImageMainDirectory = new File(root, m_ModeId+".jpg");
+            outputFileUri = Uri.fromFile(sdImageMainDirectory);
+            fOut = new FileOutputStream(sdImageMainDirectory);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error occured. Please try again later.",
+                    Toast.LENGTH_SHORT).show();
+        }
+        try {
+            bm.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+            fOut.flush();
+            fOut.close();
+
+            // Copy to downloads dir
+        } catch (Exception e) {
+        }
+
+        // For Debug, copy the file into Downloads Directory
+
+        File root = new File(getApplicationInfo().dataDir + File.separator + "pics" + File.separator);
+        File sdImageMainDirectory = new File(root, m_ModeId+".jpg");
+        File dst = new File(rootDownload, m_ModeId+".jpg");
+        if (sdImageMainDirectory.exists()) {
+            // Copy the file
+            try (InputStream in = new FileInputStream(sdImageMainDirectory)) {
+                try (OutputStream out = new FileOutputStream(dst)) {
+                    // Transfer bytes from in to out
+                    byte[] buf = new byte[1024];
+                    int len;
+                    while ((len = in.read(buf)) > 0) {
+                        out.write(buf, 0, len);
+                    }
+                }catch(Exception ex1){
+                    Log.e("Pic2Download",ex1.getMessage());
+                    ex1.printStackTrace();
+                }
+            }catch(Exception ex1){
+                Log.e("Pic2Download",ex1.getMessage());
+                ex1.printStackTrace();
             }
         }
     }
